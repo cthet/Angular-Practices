8b99269bc71295d2b74186aeffe0f962cc48a89a
cthet <clement.thet@gmail.com>
2023-02-17 00:02:07 +0100

    Add dependency injection

---
+++ Dependency Injection/di-token/src/app/services/user.service.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@angular/core';
+import { GREETER, Greeter } from '../classes/greeter.class';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  users = [
+    {
+      firstName: 'John',
+      lastName: 'Wick',
+    },
+    {
+      firstName: 'Clark',
+      lastName: 'Kent',
+    },
+    {
+      firstName: 'Peter',
+      lastName: 'Parker',
+    },
+  ];
+  constructor(@Inject(GREETER) public greeter: typeof Greeter) {}
+
+  getUser() {
+    const user = this.users[Math.floor(Math.random() * this.users.length)];
+    return new this.greeter(user);
+  }
+}
