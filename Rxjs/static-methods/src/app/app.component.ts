a7b456b3d7e6b68cf1522b7c809a899706daf01a
cthet <clement.thet@gmail.com>
2023-02-22 23:43:22 +0100

    add rxjs: unsubscribe, partition method, combinelatest operator

---
+++ Rxjs/static-methods/src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { interval, partition, merge, Subscription } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  subscription: Subscription | null = null;
+  combinedStreamData = [
+    {
+      type: 'movie',
+      title: 'john wick',
+    },
+    {
+      type: 'cartoon',
+      title: 'Naruto',
+    },
+    {
+      type: 'movie',
+      title: 'inception',
+    },
+    {
+      type: 'cartoon',
+      title: 'Dragon Ball Z',
+    },
+    {
+      type: 'cartoon',
+      title: 'Ninja Turtles',
+    },
+    {
+      type: 'movie',
+      title: 'interstellar',
+    },
+  ];
+  outputStreamData = [];
+  movies: string[] = [];
+  cartoons: string[] = [];
+
+  ngOnInit() {}
+
+  startStream() {
+    const streamSource = interval(1500).pipe(
+      map((input) => {
+        const index = input % this.combinedStreamData.length;
+        return this.combinedStreamData[index];
+      })
+    );
+    const [moviesStream, cartoonsStream] = partition(
+      streamSource,
+      (item) => item.type === 'movie'
+    );
+    this.subscription = merge(
+      moviesStream.pipe(
+        tap((movie) => {
+          this.movies.push(movie.title);
+        })
+      ),
+      cartoonsStream.pipe(
+        tap((cartoon) => {
+          this.cartoons.push(cartoon.title);
+        })
+      )
+    ).subscribe((output) => {
+      console.log(output);
+    });
+  }
+
+  stopStream() {
+    this.subscription?.unsubscribe();
+    this.subscription = null;
+  }
+}
