2468efcbeabec135d8645071dd2359eeccf4906b
cthet <clement.thet@gmail.com>
2023-03-03 16:28:45 +0100

    add template-driven-forms-validation

---
+++ Angular Forms/template-driven-forms-validation/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReleaseFormComponent } from './components/release-form/release-form.component';
+import { ReleaseLogsComponent } from './components/release-logs/release-logs.component';
+import { VersionControlComponent } from './components/version-control/version-control.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VersionControlComponent,
+    ReleaseLogsComponent,
+    ReleaseFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
