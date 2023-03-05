a97b0a48c4adaeed29e643d68f8b1522f5e9a09b
cthet <clement.thet@gmail.com>
2023-03-05 15:32:51 +0100

    add custom-form-control

---
+++ Angular Forms/custom-form-control/src/app/components/rating/rating.component.ts
@@ -0,0 +1,61 @@
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor,  NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RatingComponent),
+      multi: true,
+    },
+  ],
+})
+export class RatingComponent implements OnInit, ControlValueAccessor {
+  value = 2;
+  hoveredRating: number | null = 2;
+  isMouseOver = false;
+  @Input() disabled = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  onRatingMouseEnter(rating: number) {
+    if (this.disabled) return;
+    this.hoveredRating = rating;
+    this.isMouseOver = true;
+  }
+
+  onRatingMouseLeave() {
+    this.hoveredRating = null;
+    this.isMouseOver = false;
+  }
+
+  selectRating(rating: number) {
+    if (this.disabled) return;
+    this.value = rating;
+    this.onChange(rating);
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: number) {
+    this.value = value;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+}
