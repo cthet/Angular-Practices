020f9fd0cc5f21dabae3f7fa0d86bf5f750a9114
cthet <clement.thet@gmail.com>
2023-03-09 00:28:59 +0100

    add unit-testing-observables

---
+++ Jest/unit-testing-observables/src/app/user-detail/user-detail.component.spec.ts
@@ -0,0 +1,68 @@
+import { HttpClientModule } from '@angular/common/http';
+import {
+  waitForAsync,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteMock } from '../../__mocks__/activated-route.mock';
+import {
+  DUMMY_USERS,
+  UserServiceMock,
+} from '../../__mocks__/services/user.service.mock';
+import { UserService } from '../core/services/user.service';
+
+import { UserDetailComponent } from './user-detail.component';
+
+describe('UserDetailComponent', () => {
+  let component: UserDetailComponent;
+  let fixture: ComponentFixture<UserDetailComponent>;
+  let activatedRoute: any;
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [UserDetailComponent],
+        imports: [HttpClientModule, RouterTestingModule],
+        providers: [
+          {
+            provide: UserService,
+            useClass: UserServiceMock,
+          },
+          {
+            provide: ActivatedRoute,
+            useValue: new ActivatedRouteMock(),
+          },
+        ],
+      }).compileComponents();
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    activatedRoute = TestBed.inject(ActivatedRoute);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get the user based on routeParams on page load', fakeAsync(() => {
+    component.ngOnInit();
+    activatedRoute.setParamMap({ uuid: DUMMY_USERS[1].login.uuid });
+    tick(500);
+    expect(component.user).toEqual(DUMMY_USERS[1]);
+  }));
+
+  it('should get similar user based on routeParams uuid on page load', fakeAsync(() => {
+    component.ngOnInit();
+    activatedRoute.setParamMap({ uuid: DUMMY_USERS[0].login.uuid }); // the second user's uuid
+    const expectedSimilarUsers = DUMMY_USERS.splice(0,1); // the first user
+    tick(500);
+    expect(component.similarUsers).toEqual(expectedSimilarUsers);
+  }));
+});
